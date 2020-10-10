a2f99ca3e86b8188733f45da4701b2e82cdbdc23
sulizbasnet <sulizzbasnet@gmail.com>
2020-10-09 17:52:54 -0700

    added collision top left right bottom

--- car.js
+++ car.js
@@ -26,13 +26,35 @@ function keyUp(e) {
     keys[e.key] = false;
 }
 
+function isCollide(car, enemyCar) {
+    carRect = car.getBoundingClientRect();
+    enemyCarRect = enemyCar.getBoundingClientRect();
+
+    return !((carRect.top > enemyCarRect.bottom) || (carRect.bottom < enemyCarRect.top) ||
+        (carRect.left > enemyCarRect.right) || (carRect.right < enemyCarRect.left))
+    
+}
+
 function moveLines() {
     let lines = document.querySelectorAll('.lines');
 
     lines.forEach(function(val) {
 
         if(val.y >=850) {
-            val.y -= 750
+            val.y -= 750;
+        }
+        val.y += player.speed;
+        val.style.top = val.y + 'px';
+    })
+}
+function moveEnemyCar() {
+    let enemyCar = document.querySelectorAll('.enemy-car');
+
+    enemyCar.forEach(function(val) {
+
+        if(val.y >=850) {
+            val.y = -300;
+            val.style.left = Math.floor(Math.random() * 350) + "px";
         }
         val.y += player.speed;
         val.style.top = val.y + 'px';
@@ -42,6 +64,7 @@ function moveLines() {
 function gamePlay() {
 
     moveLines();
+    moveEnemyCar();
 
     if (player.start) {
         window.requestAnimationFrame(gamePlay);
@@ -49,7 +72,7 @@ function gamePlay() {
 
     let car = document.querySelector('.car')
     let road = gameArea.getBoundingClientRect();
-
+    console.log(road);
 
     if (keys.ArrowUp && player.y > road.top + 70) {
         player.y -= player.speed;
@@ -78,19 +101,20 @@ function startGame() {
   window.requestAnimationFrame(gamePlay);
 
   //generate lines
-  for (let i=0; i < 7; i++) {
+  for (let i=0; i < 100; i++) {
       let roadLine = document.createElement('div');
       roadLine.setAttribute('class', 'lines');
-      roadLine.y = (i*150);
+      roadLine.y = (i + 1) * 150 * -1;
       roadLine.style.top = (roadLine.y +'px');
       gameArea.appendChild(roadLine);
   }
   
   //generate cars
-  for (let i=0; i < 5; i++) {
+  for (let i=0; i < 6; i++) {
       let enemyCar = document.createElement('div');
       enemyCar.setAttribute("class", "enemy-car");
-      enemyCar.y = i * 150;
+      enemyCar.y = ((i+1)*350) * -1;
+    //   enemyCar.y = (i*150);
       enemyCar.style.top = enemyCar.y + "px";
       enemyCar.style.background = 'blue';
       enemyCar.style.left = Math.floor(Math.random() * 350) +'px';
